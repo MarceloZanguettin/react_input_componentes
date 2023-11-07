2357ec2574f8416cee107355d33c2697b39c4b5e
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-07 11:39:54 -0300

    Criado o oobjeto para os diferentes tipos de validação, para que a função validate possa fazer a validação para diferentes tipos

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const useForm = () => {
+const types = {
+  cep: {
+    regex: /^\d{5}-?\d{3}$/,
+    message: 'CEP Inválido',
+  },
+};
+
+const useForm = (type) => {
   const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  function validate(value) {
+    if (value.length === 0) {
+      setError('Preencha um valor');
+      return false;
+    } else if (!/^\d{5}-?\d{3}$/.test(value)) {
+      setError('Preencha um CEP válido');
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
 
   return {
     value,
