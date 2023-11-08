00ef8d10e2171d671c6ea34d23a7555d4912177b
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-08 08:12:51 -0300

    Reenviando App

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import Input from './Form/Input';
-import Select from './Form/Select';
-import Radio from './Form/Radio';
-import Checkbox from './Form/Checkbox';
 import useForm from './Hooks/useForm';
 
 const App = () => {
   const cep = useForm('cep');
-  /*
-  const [nome, setNome] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [produto, setProduto] = React.useState('');
-  const [cor, setCor] = React.useState('');
-  const [linguagens, setLinguagens] = React.useState([]);
-  */
+  const email = useForm('email');
+  const nome = useForm();
+  const sobrenome = useForm(false);
+
   function handleSubmit(event) {
     event.preventDefault();
     if (cep.validate() && email.validate() && nome.validate()) {
-      console.log('Enviou');
+      console.log('Enviar');
     } else {
       console.log('Não enviar');
     }
@@ -25,6 +19,8 @@ const App = () => {
 
   return (
     <form onSubmit={handleSubmit}>
+      <Input label="Nome" id="nome" type="text" {...nome} />
+      <Input label="Sobrenome" id="sobrenome" type="text" {...sobrenome} />
       <Input
         label="CEP"
         id="cep"
@@ -32,29 +28,10 @@ const App = () => {
         placeholder="00000-000"
         {...cep}
       />
+      <Input label="Email" id="email" type="email" {...email} />
       <button>Enviar</button>
     </form>
   );
-  /*
-  (
-    <form>
-      <h2>Checkbox</h2>
-      <Checkbox
-        options={['JavaScript', 'PHP', 'Python']}
-        value={linguagens}
-        setValue={setLinguagens}
-      />
-      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
-      <Select
-        options={['smartphone', 'tablet']}
-        value={produto}
-        setValue={setProduto}
-      />
-      <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
-      <Input id="email" label="Email" value={email} setValue={setEmail} />
-      <button>Enviar</button>
-    </form>
-  );*/
 };
 
 export default App;
